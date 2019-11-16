70691c5906eb08a5eee79ce46399a456571e475f
unknown <soumyakotal007@gmail.com>
2019-11-17 00:17:46 +0530

    front Desk

---
+++ src/main/java/com/frontdesk/PatientFrontDeskApplication.java
@@ -0,0 +1,13 @@
+package com.frontdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientFrontDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientFrontDeskApplication.class, args);
+	}
+
+}
